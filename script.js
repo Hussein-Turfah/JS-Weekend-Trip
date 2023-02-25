66d1f022f9c60dac398658e30c1c211e79232799
Husseinturfah <@husseinturfah>
2023-02-25 21:00:21 +0200

    Fixed some bugs.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validation(){
-const name = document.forms['document']
+const name = document.forms['registrationform']['name'].value;
 const email = document.getElementsByClassName('email');
 const password = document.getElementsByClassName('password');
-const confirmPassword = document.getElementsByClassName('confirmpassword');
+const confirm_password = document.getElementsByClassName('confirmpassword');
 const register = document.getElementById('register');
-}
+
+  console.log(name)
+    if (name == "" ||email==''||password==''||confirm_password==''){
+      alert('Please Fill in The Fields')
+      console.log('false')
+    }}//)//register end
+//}
