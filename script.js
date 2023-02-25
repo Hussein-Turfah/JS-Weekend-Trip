63f9220e51b0e9277ee1908f5d837bfd1e3db9ff
Husseinturfah <@husseinturfah>
2023-02-25 17:24:30 +0200

    Added some JS constants for form validation later on.

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+const email = document.getElementsByClassName('email');
+const password = document.getElementsByClassName('password');
+const confirmPassword = document.getElementsByClassName('confirmpassword');
+const register = document.getElementById('register');
+
+register.addEventListener('click', function(){
+console.log('1')
+})
+
+}
