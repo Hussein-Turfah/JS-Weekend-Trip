6aabc2b57fedf7138b5db2abe1a90a3339041536
Husseinturfah <@husseinturfah>
2023-02-26 14:44:21 +0200

    Fixed a bug in password verification method.

--- script.js
+++ script.js
@@ -6,28 +6,33 @@ const password = document.getElementById('password').value;
 const confirm_password = document.getElementById('confirmpassword').value;
 const register = document.getElementById('register');
 
-const password_value= "^(?=.*[A-Z])(?=.*[a-z])(?=.*[-+_!@#$% ^&*,.?]).+$" 
+const password_value= /([A-Z]|[-+_!@#$% ^&*,.?])/
+
 /*method for password verification, it means their should be one or more upper 
 case characters and one or more special characters*/
 
-if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
-  alert('Please Enter All Required Fields')
-  console.log(fname)
-  console.log(lname)
-  console.log(email)
-  console.log(password)
-  console.log(confirm_password)
-  console.log('false')
-  return false
-}
-//DO NOT FORGET EMAIL VERIFICATION!!
+// if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
+//   alert('Please Enter All Required Fields')
+//   console.log(fname)
+//   console.log(lname)
+//   console.log(email)
+//   console.log(password)
+//   console.log(confirm_password)
+//   console.log('false')
+// }
 
-if(password !== confirm_password){
-  console.log(password)
-  console.log(confirm_password)
-  console.log('password error')
+// //DO NOT FORGET EMAIL VERIFICATION!!
+
+// else if(password !== confirm_password){
+//   console.log(password)
+//   console.log(confirm_password)
+//   console.log('same password error')
+// }
+if(!password_value.test(password)){
+  console.log('password verification truemethod error')
+  alert('Your password should contain at least one special character and one upper case letter!')
 }
-if(password !== password_value||password.length<8){
-  console.log('password !== password_value||password.length<8')
-}
+// else if(password.length<8){
+//   console.log('enetr a larger password')
+// }
 }
