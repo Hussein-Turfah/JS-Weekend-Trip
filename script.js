e755387dccd3ea90e94227902f1cd7ddb19fcf4a
Husseinturfah <@husseinturfah>
2023-02-26 13:08:35 +0200

    Fixed bugs in field verification method.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function validation(){
-const name = document.forms['registrationform']['name'].value;
+const fname = document.forms['registrationform']['fname'].value;
+const lname = document.forms['registrationform']['lname'].value;
 const email = document.forms['registrationform']['email'].value;
 const password = document.forms['registrationform']['password'].value;
 const confirm_password = document.forms['registrationform']['confirmpassword'].value;
 const register = document.getElementById('register');
 
-if (name == ''||email==''||password==''||confirm_password==''){
+if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
   alert('Please Enter All Required Fields')
+  console.log(email)
+  console.log(fname)
+  console.log(lname)
+  console.log(password)
+  console.log(confirm_password)
   console.log('false')
-}}
+}
+
+}
