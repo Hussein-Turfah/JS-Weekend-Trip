10125cf7944e2d60861b2cc82475dd2b9882c45a
Husseinturfah <@husseinturfah>
2023-02-26 14:54:52 +0200

    Cleaned code and added some conditions together.

--- script.js
+++ script.js
@@ -7,32 +7,19 @@ const confirm_password = document.getElementById('confirmpassword').value;
 const register = document.getElementById('register');
 
 const password_value= /([A-Z]|[-+_!@#$% ^&*,.?])/
-
 /*method for password verification, it means their should be one or more upper 
 case characters and one or more special characters*/
 
-// if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
-//   alert('Please Enter All Required Fields')
-//   console.log(fname)
-//   console.log(lname)
-//   console.log(email)
-//   console.log(password)
-//   console.log(confirm_password)
-//   console.log('false')
-// }
-
-// //DO NOT FORGET EMAIL VERIFICATION!!
-
-// else if(password !== confirm_password){
-//   console.log(password)
-//   console.log(confirm_password)
-//   console.log('same password error')
-// }
-if(!password_value.test(password)){
-  console.log('password verification truemethod error')
-  alert('Your password should contain at least one special character and one upper case letter!')
+if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
+  alert('Please fill the remaining fields.')
 }
-// else if(password.length<8){
-//   console.log('enetr a larger password')
-// }
+//DO NOT FORGET EMAIL VERIFICATION!!
+
+else if(password !== confirm_password){
+  alert('Your passwords should be the same.')
+}
+else if(!password_value.test(password)||password.length<8){
+  alert('Your password should be at least 8 characters long with at least one special character and one upper case letter.')
+}
+return true
 }
