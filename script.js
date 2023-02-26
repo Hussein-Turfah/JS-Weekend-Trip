e44cd0951175587b72ecc06f0031b71ef2fb0319
Husseinturfah <@husseinturfah>
2023-02-26 13:45:08 +0200

    Added password verification method and fixed input value methods to use id.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 function validation(){
-const fname = document.forms['registrationform']['fname'].value;
-const lname = document.forms['registrationform']['lname'].value;
-const email = document.forms['registrationform']['email'].value;
-const password = document.forms['registrationform']['password'].value;
-const confirm_password = document.forms['registrationform']['confirmpassword'].value;
+const fname = document.getElementById('fname').value
+const lname = document.getElementById('lname').value;
+const email = document.getElementById('email').value
+const password = document.getElementById('password').value;
+const confirm_password = document.getElementById('confirmpassword').value;
 const register = document.getElementById('register');
 
 if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
   alert('Please Enter All Required Fields')
-  console.log(email)
   console.log(fname)
   console.log(lname)
+  console.log(email)
   console.log(password)
   console.log(confirm_password)
   console.log('false')
+  return false
 }
+//DO NOT FORGET EMAIL VERIFICATION!!
 
+if(password !== confirm_password||password.length<8){
+  console.log(password)
+  console.log(confirm_password)
+  console.log('password error')
+}
 }
