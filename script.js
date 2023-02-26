3dd1a080ec90a11b4abc699c931e56f453faf5e3
Husseinturfah <@husseinturfah>
2023-02-26 21:23:37 +0200

    Fixed several bugs in mergeSort function.

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-function merge(left,right){
-  let sorted_array = []
-  while (left.length && right.length){
+function merge(left, right){
+  const merged_array = []
+
+  while (left.length && right.length) {
     if (left[0]< right[0]){
-      sorted_array.push(left.shift());
+      sorted_array.push(left.shift())
     }
     else{
-      sorted_array.push(right.shift());
-  }
-return [...sorted_array, ...left, ...right];
-}}
-
-function mergeSort(array){
-  if (array.length <= 1) return array;
-
-  let mid_number = Math.floor(array.length/2)
-  let left = mergeSort(array.slice(0,mid_number))
-  let right = mergeSort(array.slice(mid_number))
-  return merge(left,right)
+      sorted_array.push(right.shift())
+  }}
+return [...sorted_array, ...left, ...right]
 }
-console.log(mergeSort([4, 5, 4, 3, 9, 8]))
+function mergeSort(unsorted_array){
+  if (unsorted_array.length < 2) {
+    return unsorted_array
+  }
+  const mid_number = Math.floor(unsorted_array.length/2)
+  const left = unsorted_array.slice(0, mid_number)
+  const right = unsorted_array.slice(mid_number)
 
-/*function validation(){
+  const left_sort = mergeSort(left)
+  const right_sort = mergeSort(right)
+
+  return merge(left_sort, right_sort)
+}
+
+
+function validation(){
 const fname = document.getElementById('fname').value;
 const lname = document.getElementById('lname').value;
 const email = document.getElementById('email').value;
@@ -31,7 +36,7 @@ const password_value= /^(?=.*[A-Z])(?=.*[-+_!@#$% ^&*,.?])/;
   /*method for password verification, it means their should be one or more upper 
   ase characters and one or more special characters*/
 
-/*if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
+if (fname == ''|| lname == ''||email==''||password==''||confirm_password==''){
   alert('Please fill the remaining fields.');
   return false;
 }
@@ -49,12 +54,9 @@ else{
   console.log('heyy')
   const numbers = prompt('10 numbers please!')
 
-  if (numbers.length=10){ 
-    console.log('true')
-    x = mergeSort(numbers)
-    console.log(x)
-
+  if (numbers.length>=10){ 
+    console.log(mergeSort(numbers))
   }
   else{
-  return false
-}}}*/
+  console.log('false')
+}}}
